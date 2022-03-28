05bcc4cf22c1140629a3b857458fb6bac29dca8d
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-28 20:09:53 +0300

    Heroku configuration updated

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ from suds.client import Client
 app = Flask("__name__")
 client = Client('http://localhost:8090/?wsdl', cache=None)
 
+
+@app.route('/')
+def index():
+    return "It's working!"
+
+
 @app.route('/showip', methods=['GET'])
 def host():
     domain_name = request.args.get('domain_name')
     if domain_name is None:
         domain_name = 'google.com'
     return client.service.res_name(domain_name)
-
 
 
 if __name__ == "__main__":

bb682f343e78007287f91168446988444cb9556b
lukasbaltramaitis <lukasbaltramaitis@gmail.com>
2022-03-28 19:53:08 +0300

    Host client changes added

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, request
 from suds.client import Client
 
 app = Flask("__name__")
 client = Client('http://localhost:8090/?wsdl', cache=None)
 
-@app.route('/')
-def index():
-    return client.service.HelloWorldService()
+@app.route('/showip', methods=['GET'])
+def host():
+    domain_name = request.args.get('domain_name')
+    if domain_name is None:
+        domain_name = 'google.com'
+    return client.service.res_name(domain_name)
+
 
 
 if __name__ == "__main__":

349c7d9aceda1a4eaec359946c5585fbc2ac50e6
PauliusKu <paulius.kuprevicius@gmail.com>
2022-03-28 20:30:30 +0300

    added dns

--- app.py
+++ app.py
@@ -22,5 +22,13 @@ def host():
     return client.service.res_name(domain_name)
 
 
+@app.route('/dns', methods=['GET'])
+def host():
+    global client
+    domain_name = request.args.get('domain_name')
+    if domain_name is None:
+        domain_name = 'google.com'
+    return client.service.more_info(domain_name)
+
 if __name__ == "__main__":
     app.run(debug=True)

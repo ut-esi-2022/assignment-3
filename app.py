447dd547016439f2cc4d4c425b37d2e6472a2a84
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-28 20:27:39 +0300

    Adding back SOAP-related functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 from suds.client import Client
 
 app = Flask("__name__")
-# client = Client('http://localhost:8090/?wsdl', cache=None)
+client = Client('http://localhost:8090/?wsdl', cache=None)
 server_uri = os.environ.get('SERVER_URI')
 
 @app.route('/')
@@ -13,12 +13,13 @@ def index():
     return f"Server URL: {server_uri}"
 
 
-# @app.route('/showip', methods=['GET'])
-# def host():
-#     domain_name = request.args.get('domain_name')
-#     if domain_name is None:
-#         domain_name = 'google.com'
-#     return client.service.res_name(domain_name)
+@app.route('/showip', methods=['GET'])
+def host():
+    global client
+    domain_name = request.args.get('domain_name')
+    if domain_name is None:
+        domain_name = 'google.com'
+    return client.service.res_name(domain_name)
 
 
 if __name__ == "__main__":

34fff2abf68535f2d0249224a0fedc3cb3bf2ebe
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-29 15:58:29 +0300

    Renaming some functions and removing unnecessary docstrings

--- soap_server.py
+++ soap_server.py
@@ -11,43 +11,23 @@ import socket
 import dns 
 import dns.resolver
 
-class HelloWorldService(ServiceBase):
-
-    @rpc(Unicode, Integer, _returns=Iterable(Unicode))
-    def say_hello(ctx, name, times):
-        """Docstrings for service methods appear as documentation in the wsdl.
-        <b>What fun!</b>
-        @param name the name to say hello to
-        @param times the number of times to say hello
-        @return the completed array
-        """
-
-        for i in range(times):
-            yield u'Tere, %s' % name
-    
+class NetworkToolsService(ServiceBase):
     @rpc(Unicode, _returns=Unicode)
     def ping_host(ctx, domain_name):
         return os.popen(f'ping -c 2 {domain_name}').read()
 
     @rpc(Unicode, _returns=Unicode)
-    def res_name(ctx, domain_name):
-        """Docstrings for service methods appear as documentation in the wsdl.
-        <b>What fun!</b>
-        @param domain_name the domain name to call host command on
-        @return the return of host command
-        """
+    def dns_lookup(ctx, domain_name):
         return os.popen(f'host {domain_name}').read()
 
-
     @rpc(Unicode, _returns=Unicode)
-    def more_info(ctx, domain_name):
+    def dig_more(ctx, domain_name):
         ns = os.popen(f'dig NS {domain_name}').read()
         mx = os.popen(f'dig MX {domain_name}').read()
         soa = os.popen(f'dig SOA {domain_name}').read()
-
         return  "NS:\n" + ns + "\nMX:\n" + mx + "\nSOA:\n" + soa
 
-application = Application([HelloWorldService], 'spyne.examples.hello.soap',
+application = Application([NetworkToolsService], 'spyne.examples.hello.soap',
                           in_protocol=Soap11(validator='lxml'),
                           out_protocol=Soap11())
 

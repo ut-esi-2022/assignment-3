7a85a7a2f9f5d88aeda8cd7eb796d8848ddb4e51
Ihar Suvorau <ihar.suvorau@gmail.com>
2022-03-29 16:12:26 +0300

    Client updated

--- soap_client.py
+++ soap_client.py
@@ -2,14 +2,9 @@ from suds.client import Client
 
 client = Client('http://localhost:8090/?wsdl', cache=None)
 
-print(client.service.say_hello(u'Maarika', 5))
 print("Running Service 1 ...")
 print(client.service.ping_host('www.facebook.com'))
 print("Running Service 2 ...")
-print(client.service.res_name('www.google.com'))
+print(client.service.dns_lookup('www.google.com'))
 print("\nRunning Service 3 ...")
-print(client.service.more_info('www.err.ee'))
-
-# wsdl = Client('http://www.learnwebservices.com/services/hello?wsdl', cache=None)
-# request = {'Name': 'Maarika'}
-# print(wsdl.service.SayHello(request))
+print(client.service.dig_more('www.err.ee'))
